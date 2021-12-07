c078eb674bfae0b7c92dcf447727ca894cd70eb2
LAnderson-o <lca98620@gmail.com>
2021-12-07 11:29:17 -0800

    private enemylist

--- PA9.4/Enemies.cpp
+++ PA9.4/Enemies.cpp
@@ -6,15 +6,15 @@ Enemies::Enemies(){
 }
 
 void Enemies::spawnEnemy(sf::RenderWindow* window) {
-	Enemy enemy1(enemyid);
+	Enemy *enemy1 = new Enemy(enemyid);
 	enemyid++;
 	int randy = rand() % window->getSize().y/2;
 	int randx = rand() % window->getSize().x;
-	enemy1.setPosition(randx, randy);
+	enemy1->setPosition(randx, randy);
 	//cout << enemy1.getPosition().x << " " << enemy1.getPosition().y << endl;
-	enemy1.setTexture(enemyTexture);
-	enemy1.gX = rand() % window->getSize().x;
-	enemy1.gY = rand() % window->getSize().y;
+	enemy1->setTexture(enemyTexture);
+	enemy1->gX = rand() % window->getSize().x;
+	enemy1->gY = rand() % window->getSize().y;
 	enemyList.push_back(enemy1);
 }
 
@@ -26,20 +26,20 @@ void Enemies::despawnEnemy() {
 void Enemies::update(sf::RenderWindow* window, sf::FloatRect bounds) {
 	//loop through all enemies check if bullet intersects enemy or enemy intersects player
 	for (int i = 0; i < enemyList.size(); ++i) {
-		if (enemyList[i].getPosition().x == enemyList[i].gX && enemyList[i].getPosition().y == enemyList[i].gY) {
+		if (enemyList[i]->getPosition().x == enemyList[i]->gX && enemyList[i]->getPosition().y == enemyList[i]->gY) {
 			//reached goal
 			//assign new goal
-			enemyList[i].gX = rand() % window->getSize().x;
-			enemyList[i].gY = rand() % window->getSize().y;
-		} else if (!bounds.contains(enemyList[i].getPosition())) {
-			enemyList[i].gX = rand() % window->getSize().x;
-			enemyList[i].gY = rand() % window->getSize().y;
+			enemyList[i]->gX = rand() % window->getSize().x;
+			enemyList[i]->gY = rand() % window->getSize().y;
+		} else if (!bounds.contains(enemyList[i]->getPosition())) {
+			enemyList[i]->gX = rand() % window->getSize().x;
+			enemyList[i]->gY = rand() % window->getSize().y;
 		} else {
-			float slope = enemyList[i].gY - enemyList[i].getPosition().y / enemyList[i].gX - enemyList[i].getPosition().x;
+			float slope = enemyList[i]->gY - enemyList[i]->getPosition().y / enemyList[i]->gX - enemyList[i]->getPosition().x;
 			/*enemyList[i].mX = sin(slope);
 			enemyList[i].mY = cos(slope);
 			cout << sin(slope) << " " << cos(slope) << endl;*/
-			enemyList[i].move(sin(slope), cos(slope));
+			enemyList[i]->move(sin(slope), cos(slope));
 		}
 		//calculate mx and my
 
