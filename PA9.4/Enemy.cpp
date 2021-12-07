5b615353bafa07f25590e96c46b6a41bf7cd1fc9
Matthew Mcintosh <matthew.l.mcintosh@wsu.edu>
2021-12-07 12:47:10 -0800

    movement update

--- PA9.4/Enemy.cpp
+++ PA9.4/Enemy.cpp
@@ -27,17 +27,45 @@ Enemy::Enemy(int newID, Vector2f& pos, Vector2f& nGoal)
 
 void Enemy::movement(sf::RenderWindow* window, sf::FloatRect bounds, float &dt)
 {
-	if (this->getPosition() == goal)
-	{
-		goal.x = (rand() % window->getSize().x);
+	if ((this->getPosition() - goal).x > 0 && (this->getPosition() - goal).x < 10) //when fly is 'at' goal pos
+	{//had to make it 'near' as it would just jump over goal
+		goal.x = (rand() % window->getSize().x); //generate new goal
 		goal.y = (rand() % window->getSize().y);
+
+		float n1;
+		mov = goal - getPosition(); 
+		if (mov.x < 0) {
+			n1 = atan(mov.y / mov.x) + 3.14159265;
+		}
+		else {
+			n1 = atan(mov.y / mov.x);
+		}
+		float n2 = cos(n1);
+		float n3 = sin(n1);
+		mov.x = n2;
+		mov.y = n3;
 	}
 
-	else if (!bounds.contains(this->getPosition()))
-	{
-		goal.x = (rand() % window->getSize().x);
-		goal.y = (rand() % window->getSize().y);
-	}
+	//else if (!bounds.contains(this->getPosition())) //when fly is at walls, prolly dont need
+	//{
+	//	goal.x = (rand() % window->getSize().x);
+	//	goal.y = (rand() % window->getSize().y); 
+
+
+
+	//	float n1;
+	//	mov = goal - getPosition();
+	//	if (mov.x < 0) {
+	//		n1 = atan(mov.y / mov.x) + 3.14159265;
+	//	}
+	//	else {
+	//		n1 = atan(mov.y / mov.x);
+	//	}
+	//	float n2 = cos(n1);
+	//	float n3 = sin(n1);
+	//	mov.x = n2;
+	//	mov.y = n3;
+	//}
 	else
 	{
 		move((float) speed*mov*dt);
