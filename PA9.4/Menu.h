e5dbddbd0cfb7ea3929ecd0bc2d45b6010042b96
LAnderson-o <lca98620@gmail.com>
2021-12-07 19:13:04 -0800

    .

---
+++ PA9.4/Menu.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <iostream>
+#include "SFML/Graphics.hpp"
+
+#define NUM_OF_ITEMS 3
+class Menu
+{
+public:
+	//constructor & deconstructor
+	Menu(float width, float height);
+	~Menu();
+
+	//functions
+	void draw(sf::RenderWindow& window);
+	void MoveUp();
+	void MoveDown();
+
+private:
+	int itemIndex;
+	sf::Font font;
+	sf::Text menu[NUM_OF_ITEMS];
+};
+
+
