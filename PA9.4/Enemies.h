3e1a793fb67e780f78c096d64db87ed83e1b5797
LAnderson-o <lca98620@gmail.com>
2021-12-07 19:21:03 -0800

    .

--- PA9.4/Enemies.h
+++ PA9.4/Enemies.h
@@ -7,12 +7,12 @@ public:
 	Enemies();
 	~Enemies();
 
-	void spawnEnemy(sf:: RenderWindow* window);
+	void spawnEnemy(sf::RenderWindow* window);
 
 	void despawnEnemy();
 	//despawn condition and event (score etc)
 	void update(sf::RenderWindow* window, sf::FloatRect bounds,
-		float& dt, vector<Bullet*>& pBull, 
+		float& dt, vector<Bullet*>& pBull,
 		vector<Bullet*>& eBull, Vector2f pPos, int& score);
 	//fires gun and moves enemies
 	void render(sf::RenderWindow* window);
@@ -22,8 +22,8 @@ private:
 	std::vector<Enemy*> enemyList;
 
 	sf::Texture enemyTexture;
-	
-	
+
+
 	int enemyid;
 
 };
